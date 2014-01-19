0d96af915d61c893dc86764755655cf33abfa88e
EAMON MC CANN <10348373@brn004-linux.(none)>
2014-01-19 14:33:08 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = '59f51f1be36c18e7f7576fc126e76ffbc4071a74e6cc936e341338b3a7529e1c04cde363eec26904fa113ee21a46edb56cd6e6b5a835b5f53ef3bdbdecf7c84b'
